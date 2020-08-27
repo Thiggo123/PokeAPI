d37baab91cdf06d16b962e92a98e87917e634d9a
thiggo123 <thiggo123@hotmail.com>
2020-08-27 08:55:08 -0300

    PrimeiroCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SCP-API';
-}
+  title = 'POKE-API';
+  
+  
+
+
+public scp$: Observable<any>;
+
+  constructor(private http: HttpClient){
+
+
+  }
+
+  
+
+  OnInput(event: any) {
+    var input = event.target.value;
+
+    if (input == "SUAMAE") {
+      var url = "https://pokeapi.co/api/v2/pokemon/snorlax";
+
+      this.http.get(url).subscribe(d=> console.log(d));
+      
+      this.scp$ = this.http.get(url);
+      
+    }else{
+
+      var url = "https://pokeapi.co/api/v2/pokemon/"+input;
+
+      this.http.get(url).subscribe(d=> console.log(d));
+      
+      this.scp$ = this.http.get(url);
+      
+    }
+
+    
+
+    }
+  
+  }
+
+
+
